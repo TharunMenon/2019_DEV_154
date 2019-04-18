ab2ee152087545267978b558580e79daf57bddc1
Tharun <tharunrajmenon@gmail.com>
2019-04-18 14:18:48 +0800

    Total Score

--- Bowling/BowlingController.swift
+++ Bowling/BowlingController.swift
@@ -10,17 +10,59 @@ import Foundation
 
 final class BowlingController {
     
+    private var ballRolls = [Int](repeating: 0, count: 21)
+    private var currentballRoll = 0
+    
+    
     // MARK: - Function to call each time when ball rolled
     
     func ballroll(_ pins: Int) {
+        ballRolls[currentballRoll] = pins
+        currentballRoll += 1
         
     }
     
     // MARK: - Function to get total score
     
-    func toatalscore() -> Int {
+    func totalscore() -> Int {
         
-        return 0
+        var score = 0
+        var ballRoll = 0
+        for _ in 1...10 {
+            if (ballStrike(ballRoll)) {
+                score += 10 + strikeBallBonus(ballRoll)
+                ballRoll += 1
+            } else if ballSpare(ballRoll) {
+                score += 10 + spareBallBonus(ballRoll)
+                ballRoll += 2
+            } else {
+                score += sumOfBall(ballRoll)
+                ballRoll += 2
+            }
+        }
+        return score
     }
+    
+    private func ballStrike(_ ballRoll: Int) -> Bool {
+        return ballRolls[ballRoll] == 10
+    }
+    
+    private func ballSpare(_ ballRoll: Int) -> Bool {
+        return ballRolls[ballRoll] + ballRolls[ballRoll + 1] == 10
+    }
+    
+    private func strikeBallBonus(_ ballRoll: Int) -> Int {
+        return ballRolls[ballRoll + 1] + ballRolls[ballRoll + 2]
+    }
+    
+    private func spareBallBonus(_ ballRoll: Int) -> Int {
+        return ballRolls[ballRoll + 2]
+    }
+    
+    private func sumOfBall(_ ballRoll: Int) -> Int {
+        return ballRolls[ballRoll] + ballRolls[ballRoll + 1]
+    }
+    
+    
     
 }
--- BowlingTests/BowlingTests.swift
+++ BowlingTests/BowlingTests.swift
@@ -11,6 +11,61 @@ import XCTest
 
 class BowlingTests: XCTestCase {
 
+    private var bowlingGame: BowlingController!
+    override func setUp() {
+        super.setUp()
+        bowlingGame = BowlingController()
+    }
+    
+    override func tearDown() {
+        bowlingGame = nil
+        super.tearDown()
+    }
+    
+    private func rollBallMany(pins: Int, times: Int) {
+        for _ in 1...times {
+            bowlingGame.ballroll(pins)
+        }
+    }
+    
+    func testGame() {
+        rollBallMany(pins: 0, times: 20)
+        XCTAssertEqual(bowlingGame.totalscore(), 0)
+    }
+    
+    func testAllOnes() {
+        rollBallMany(pins: 1, times: 20)
+        XCTAssertEqual(bowlingGame.totalscore(), 20)
+    }
+    
+    private func rollBallStrike() {
+        bowlingGame.ballroll(10)
+    }
+    
+    private func rollBallSpare() {
+        bowlingGame.ballroll(5)
+        bowlingGame.ballroll(5)
+    }
+    
+    func testOneBallSpare() {
+        rollBallSpare()
+        bowlingGame.ballroll(3)
+        rollBallMany(pins: 0, times: 17)
+        XCTAssertEqual(bowlingGame.totalscore(), 16)
+    }
+    
+    func testOneBallStrike() {
+        rollBallStrike()
+        bowlingGame.ballroll(3)
+        bowlingGame.ballroll(4)
+        rollBallMany(pins: 0, times: 16)
+        XCTAssertEqual(bowlingGame.totalscore(), 24)
+    }
+    
+    func testActualGame() {
+        rollBallMany(pins: 10, times: 12)
+        XCTAssertEqual(bowlingGame.totalscore(), 300)
+    }
    
 
 }
